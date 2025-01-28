5bacb2e59c9756b67da84883c2ca8dee0ec6c0cb
Karma63 <adam2004buchan@outlook.com>
2025-01-28 18:35:22 +0000

    Fix token placeholder in start.sh for git clone command

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ node --version
 sudo apt install curl
 curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash 
 sudo apt-get install -y nodejs
-git clone https://AdamB64:(github token)@github.com/AdamB64/Project.git
+git clone https://AdamB64:(githubtoken)@github.com/AdamB64/Project.git
 cd project
 npm start
