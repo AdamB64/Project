95c59fe579a08110dc234074a9e2ab37e21c298c
Karma63 <adam2004buchan@outlook.com>
2025-01-29 12:03:06 +0000

    Fix case sensitivity in directory name in start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ sudo apt install curl
 curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash 
 sudo apt-get install -y nodejs
 git clone https://AdamB64:(githubtoken)@github.com/AdamB64/Project.git
-cd project
+cd Project
 npm start
