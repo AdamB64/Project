829df5d256d9030e044adb15a8a757bcaf38cf23
Karma63 <adam2004buchan@outlook.com>
2025-02-01 19:40:34 +0000

    Add ls command to start.sh for improved visibility during setup

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ sudo apt install curl
 curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash 
 sudo apt-get install -y nodejs
 git clone https://AdamB64:(githubtoken)@github.com/AdamB64/Project.git
+ls
 cd Project
 npm ci
 npm start
