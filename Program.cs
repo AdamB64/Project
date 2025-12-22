6224cc735fa8baadf8130529795f8ae3a3128440
AdamB64 <adam2004buchan@outlook.com>
2025-12-22 22:28:36 +0000

    just getting stared with api calls using C#

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -9,10 +10,16 @@ var app = builder.Build();
 // static files from wwwroot
 app.UseStaticFiles();
 
+// serve html
 app.MapGet("/", async context =>
 {
     await context.Response.SendFileAsync("wwwroot/html/example.html");
 });
 
+// simple API endpoint that returns "hello"
+app.MapGet("/api/hello", () =>
+{
+    return Results.Ok("hello");
+});
 
 app.Run();
--- wwwroot/js/about.js
+++ wwwroot/js/about.js
@@ -1,1 +1,15 @@
 console.log("About page script loaded!");
+
+const response = fetch('/api/hello')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to send data to the server');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Response from server:', data);
+    }
+    ).catch(error => {
+        console.error('Error:', error);
+    });
